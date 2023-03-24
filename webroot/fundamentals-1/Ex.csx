94c21f5970b3e8861f27f393155a649bca890e70
adam fraser-kruck <adamfk@gmail.com>
2023-03-23 21:21:05 -0500

    add initial working examples!

---
+++ webroot/fundamentals-1/Ex.csx
@@ -0,0 +1,151 @@
+#!/usr/bin/env dotnet-script
+#r "nuget: StateSmith, 0.8.10-alpha"
+
+// This is a C# script file. Very useful for running StateSmith.
+// https://github.com/StateSmith/StateSmith/wiki/Using-c%23-script-files-(.CSX)-instead-of-solutions-and-projects
+
+using System.Text.RegularExpressions;
+using StateSmith.Input.Expansions;
+using StateSmith.Output;
+using StateSmith.Output.UserConfig;
+using StateSmith.Runner;
+using StateSmith.Common;
+using StateSmith.SmGraph.Visitors;
+using StateSmith.SmGraph;
+using StateSmith.Output.Gil;
+using StateSmith.Input.DrawIo;
+
+GenEx01();
+GenEx10();
+
+static void GenEx01()
+{
+    SmRunner runner = new(diagramPath: "Ex01.drawio.svg", new Ex10Glue(), transpilerId: TranspilerId.JavaScript);
+    AddTransformers(runner);
+    runner.Run();
+}
+
+static void GenEx10()
+{
+    SmRunner runner = new(diagramPath: "Ex10.drawio.svg", new Ex10Glue(), transpilerId: TranspilerId.JavaScript);
+    AddTransformers(runner);
+    runner.Settings.propagateExceptions = true;
+    runner.Run();
+}
+
+static void AddTransformers(SmRunner runner)
+{
+    TracingModder tm = new();
+    runner.SmTransformer.InsertAfterFirstMatch(StandardSmTransformer.TransformationId.Standard_Validation1, new TransformationStep(id: nameof(MyTracingModder), action: (sm) => new MyTracingModder().AddTracingBehaviors(sm)));
+    runner.SmTransformer.InsertBeforeFirstMatch(StandardSmTransformer.TransformationId.Standard_SupportOrderAndElse, new TransformationStep(id: "", FixUpNonTransitionBehaviorIds));
+}
+
+// needs to happen before behavior re-ordering!
+static void FixUpNonTransitionBehaviorIds(StateMachine sm)
+{
+    sm.VisitTypeRecursively<NamedVertex>(v => {
+        var list = v.DiagramSubIds.ToList();
+        foreach (var b in v.Behaviors)
+        {
+            if (b.DiagramId == "")
+            {
+                b.DiagramId = list[0];
+                list.RemoveAt(0);
+            }
+        }
+    });
+}
+
+//-------------------------------------------------------
+
+// ignore C# guidelines for script stuff below
+#pragma warning disable IDE1006, CA1050
+
+
+/// <summary>
+/// This class is read by StateSmith to determine configuration and expansions.
+/// </summary>
+public class Ex10Glue : IRenderConfigJavaScript
+{
+    string IRenderConfigJavaScript.ExtendsSuperClass => "Ex10Base";
+
+    string IRenderConfig.AutoExpandedVars => @"
+        count: 0,
+    ";
+
+    /// <summary>
+    /// These expansions allow you to write clear concise code in diagrams.
+    /// More info: https://github.com/StateSmith/StateSmith/blob/main/docs/diagram-features.md#write-code-directly-or-use-expansions
+    /// </summary>
+    public class MyExpansions : UserExpansionScriptBase
+    {
+        public string trace_action(string actionCode, string behaviorId, string umlDescription) => $"this.{AutoNameCopy()}({actionCode}, {behaviorId}, {umlDescription})";
+        public string trace_transition(string behaviorId) => $"this.{AutoNameCopy()}({behaviorId})";
+        public string trace_enter(string stateName, string id) => $"this.{AutoNameCopy()}({stateName}, {id})";
+        public string trace_exit(string stateName, string id) => $"this.{AutoNameCopy()}({stateName}, {id})";
+
+        public string light_off() => $"this.{AutoNameCopy()}()";
+        public string light_blue() => $"this.{AutoNameCopy()}()";
+        public string light_yellow() => $"this.{AutoNameCopy()}()";
+        public string light_boom() => $"this.{AutoNameCopy()}()";
+    }
+}
+
+
+
+public class MyTracingModder : NamedVisitor
+{
+    public void AddTracingBehaviors(StateMachine stateMachine)
+    {
+        Visit(stateMachine);
+    }
+
+    public override void Visit(Vertex v)
+    {
+        foreach (var b in v.Behaviors)
+        {
+            if (b.HasTransition())
+            {
+                b.actionCode = $"trace_transition(\"{b.DiagramId}\");" + b.actionCode;
+            }
+            else if (b.HasActionCode())
+            {
+                var uml = b.DescribeAsUml();
+                string escapedUml = EscapeCharsForString(uml);
+
+                b.actionCode = $"trace_action(\"{EscapeCharsForString(b.actionCode)}\", \"{b.DiagramId}\", \"{escapedUml}\");" + b.actionCode;
+            }
+        }
+
+        VisitChildren(v);
+    }
+
+    public override void Visit(NamedVertex v)
+    {
+        Visit((Vertex)v);
+
+        if (v is not StateMachine)
+        {
+            v.AddBehavior(index: 0, behavior: new Behavior(trigger: TriggerHelper.TRIGGER_ENTER, guardCode: null, actionCode: $"trace_enter(\"{v.Name}\", \"{v.DiagramId}\");"));
+            v.AddBehavior(index: 0, behavior: new Behavior(trigger: TriggerHelper.TRIGGER_EXIT, guardCode: null, actionCode: $"trace_exit(\"{v.Name}\", \"{v.DiagramId}\");"));
+        }
+    }
+
+    public override void Visit(NotesVertex v)
+    {
+        // ignore
+    }
+
+    public override void Visit(InitialState v)
+    {
+        Visit((Vertex)v);
+    }
+
+    static string EscapeCharsForString(string str)
+    {
+        if (str == null) return null;
+
+        str = Regex.Replace(str, @"(\\|"")", "\\$1");
+        return str;
+    }
+}
---
+++ webroot/fundamentals-1/Ex01Base.js
@@ -0,0 +1,211 @@
+class ExBase {
+    /** @type {Document} */
+    smSvgDoc;
+
+    /** @type {Map<string, function>} */
+    tempHighlightMap = new Map();
+
+    /** @type {Map<string, function>} */
+    stateHighlightMap = new Map();
+
+    /**
+     * @type {number}
+     */
+    autoClearTimeoutId;
+
+    clearTempHighlights() {
+        this.tempHighlightMap.forEach((restoreFunction) => {
+            restoreFunction();
+        });
+
+        this.tempHighlightMap.clear();
+    }
+
+    /**
+     * @param {Map<string, function>} map
+     * @param {string} key
+     */
+    undoChangesForKey(map, key) {
+        let undoFunction = map.get(key);
+        undoFunction();
+        map.delete(key);
+    }
+
+    /**
+     * @param {string} actionCode
+     * @param {string} behaviorId
+     * @param {string} vertexId
+     */
+    trace_action(actionCode, behaviorId, vertexId) {
+        console.log(`Action: ${actionCode}, ${behaviorId}, ${vertexId}`);
+
+        let containingObj = this.getSvgElementFromDiagramId(behaviorId);
+
+        /** @type {NodeListOf<HTMLDivElement>} */
+        let divs = containingObj.querySelectorAll("div[data-drawio-colors] > div");
+
+        /** @type {HTMLDivElement} */
+        let div = divs[0];
+
+        if (!div)
+            return; // happens if no text
+
+        let key = behaviorId + "-stroke";
+
+        let old = this.tempHighlightMap.get(key);
+        if (!old) {
+            let oldBg = div.style.backgroundColor;
+            this.tempHighlightMap.set(key, () => div.style.backgroundColor = oldBg);
+            div.style.backgroundColor = "#3b3b3b";
+        }
+    }
+
+    /**
+     * @param {string} behaviorId
+     */
+    trace_transition(behaviorId) {
+        console.log(`Transition: ${behaviorId}`);
+
+        this.trace_action("", behaviorId, "");
+        this.highlightPaths(this.tempHighlightMap, behaviorId, true);
+    }
+
+    /**
+     * @param {string} stateName
+     * @param {string} stateId
+     */
+    trace_enter(stateName, stateId) {
+        console.log(`entered "${stateName}", "${stateId}"`);
+        this.highlightPaths(this.stateHighlightMap, stateId, false);
+    }
+
+    /**
+     * @param {string} stateName
+     * @param {string} stateId
+     */
+    trace_exit(stateName, stateId) {
+        console.log(`exited "${stateName}", "${stateId}"`)
+        this.restorePaths(stateId);
+    }
+
+    /**
+     * @param {string} diagramId
+     */
+    makePathsKey(diagramId) {
+        let key = diagramId + "-paths";
+        return key;
+    }
+
+    /**
+     * @param {Map<string, Function>} restoreMap
+     * @param {string} diagramId
+     * @param {boolean} shouldFill
+     */
+    highlightPaths(restoreMap, diagramId, shouldFill) {
+        const stateDom = this.getSvgElementFromDiagramId(diagramId);
+        let paths = stateDom.querySelectorAll("path");
+
+        let key = this.makePathsKey(diagramId);
+        let old = restoreMap.get(key);
+        if (old)
+            return;
+
+        let highlightColor = "#dc3545";
+
+        paths.forEach(o => {
+            this.setAttributeWithRestore(o, "stroke", highlightColor);
+            this.setAttributeWithRestore(o, "stroke-width", "2px");
+            if (shouldFill)
+                this.setAttributeWithRestore(o, "fill", highlightColor);
+        });
+
+        restoreMap.set(key, () => {
+            paths.forEach(o => {
+                this.restoreOldAttribute(o, "stroke");
+                this.restoreOldAttribute(o, "stroke-width");
+                if (shouldFill)
+                    this.restoreOldAttribute(o, "fill");
+            });
+        });
+    }
+
+    /**
+     * @param {string} diagramId
+     */
+    restorePaths(diagramId) {
+        let key = this.makePathsKey(diagramId);
+        this.undoChangesForKey(this.stateHighlightMap, key);
+    }
+
+    /**
+     * @param {string} stateId
+     */
+    getSvgElementFromDiagramId(stateId) {
+        return this.smSvgDoc.getElementById("cell-" + stateId);
+    }
+
+    /**
+     * @param {string} attributeName
+     */
+    makeOldAttrName(attributeName) {
+        return attributeName + "-ss-old";
+    }
+
+    /**
+     * @param {HTMLObjectElement|SVGPathElement} object
+     * @param {string} attributeName
+     * @param {string} newValue
+     */
+    setAttributeWithRestore(object, attributeName, newValue) {
+        let old = object.getAttribute(attributeName);
+        object.setAttribute(this.makeOldAttrName(attributeName), old);
+        object.setAttribute(attributeName, newValue);
+    }
+
+    /**
+     * @param {HTMLObjectElement|SVGPathElement} object
+     * @param {string} attributeName
+     */
+    restoreOldAttribute(object, attributeName) {
+        let old = object.getAttribute(this.makeOldAttrName(attributeName));
+        object.setAttribute(attributeName, old);
+    }
+}
+
+class Ex10Base extends ExBase {
+    /** @type {Document} */
+    lightBulbSvgDoc;
+
+    /**
+     * @param {string} color
+     * @param {string=} strokeColor
+     */
+    setLightBulbColor(color, strokeColor) {
+        strokeColor = strokeColor || color;
+        let paths = this.lightBulbSvgDoc.querySelectorAll("path");
+        paths.forEach(element => {
+            element.setAttribute("fill", color);
+            element.setAttribute("stroke", strokeColor);
+        });
+    }
+
+    light_off() {
+        console.log("light off");
+        this.setLightBulbColor("#000000");
+    }
+
+    light_blue() {
+        console.log("light blue");
+        this.setLightBulbColor("#2ca7ff");
+    }
+
+    light_yellow() {
+        console.log("light yellow");
+        this.setLightBulbColor("yellow", "black");
+    }
+
+    light_boom() {
+        console.log("light boom");
+        this.setLightBulbColor("red");
+    }
+}
